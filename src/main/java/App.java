5e63c1529eedab4ab53aabacd324ce15f1d9e687
Jeffrey Ruder and Kyle Misener <km.pacific.27@gmail.com>
2016-01-07 15:55:37 -0800

    Add deposit coins functionality

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,11 +10,57 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
-    ChangeMachine bankOfEpicodus = new ChangeMachine(100, 100, 100);
+    ChangeMachine bankOfEpicodus = new ChangeMachine(30, 30, 30);
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
+
+      Integer quartersLeft = bankOfEpicodus.getQuarters();
+      Integer dimesLeft = bankOfEpicodus.getDimes();
+      Integer nickelsLeft = bankOfEpicodus.getNickels();
+      String penniesLeft = "The Bank of Epicodus has infinite pennies!";
+
+      model.put("quartersLeft", quartersLeft);
+      model.put("dimesLeft", dimesLeft);
+      model.put("nickelsLeft", nickelsLeft);
+      model.put("penniesLeft", penniesLeft);
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/technician-entry", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/technician-entry.vtl");
+
+      Integer quartersLeft = bankOfEpicodus.getQuarters();
+      Integer dimesLeft = bankOfEpicodus.getDimes();
+      Integer nickelsLeft = bankOfEpicodus.getNickels();
+      String penniesLeft = "The Bank of Epicodus has infinite pennies!";
+
+      model.put("quartersLeft", quartersLeft);
+      model.put("dimesLeft", dimesLeft);
+      model.put("nickelsLeft", nickelsLeft);
+      model.put("penniesLeft", penniesLeft);
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/technician-display", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/technician-display.vtl");
+
+      Integer quartersDeposited = Integer.parseInt(request.queryParams("depositqtr"));
+      Integer dimesDeposited = Integer.parseInt(request.queryParams("depositdim"));
+      Integer nickelsDeposited = Integer.parseInt(request.queryParams("depositnic"));
+
+      bankOfEpicodus.addQuarters(quartersDeposited);
+      bankOfEpicodus.addDimes(dimesDeposited);
+      bankOfEpicodus.addNickels(nickelsDeposited);
+
+      model.put("Quarters", quartersDeposited);
+      model.put("Dimes", dimesDeposited);
+      model.put("Nickels", nickelsDeposited);
 
       Integer quartersLeft = bankOfEpicodus.getQuarters();
       Integer dimesLeft = bankOfEpicodus.getDimes();
